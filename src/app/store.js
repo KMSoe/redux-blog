f7eccd34d76627c296d35d163340dafbe5cb9bc9
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-09-11 20:29:32 +0630

    fix

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { useReducer } from 'react'
 import postsSlice from './features/posts/postsSlice'
+import usersSlice from './features/users/usersSlice'
 
 export default configureStore({
   reducer: {
     posts: postsSlice,
-    users: useReducer
+    users: usersSlice
   }
 })
