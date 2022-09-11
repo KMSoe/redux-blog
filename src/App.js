b6d6a4478a33020369e2d72d9df07a4eb4cc998d
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-09-11 19:45:41 +0630

    link to PostDetail

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import { Navbar } from './app/Navbar'
 import PostsList from './app/features/posts/PostsList'
 import AddPostForm from './app/features/posts/AddPostForm'
+import SinglePostPage from './app/features/posts/SinglePostPage'
 
 function App() {
   return (
@@ -26,6 +27,8 @@ function App() {
               </section>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+
           <Redirect to="/" />
         </Switch>
       </div>
