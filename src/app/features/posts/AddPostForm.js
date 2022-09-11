ecba3e6f105036e6698cc3d055923abc9a39a7ab
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-09-11 20:21:58 +0630

    fix

--- src/app/features/posts/AddPostForm.js
+++ src/app/features/posts/AddPostForm.js
@@ -1,4 +1,3 @@
-import { nanoid } from "@reduxjs/toolkit";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { postAdded } from "./postsSlice";
@@ -13,13 +12,9 @@ const AddPostForm = () => {
 
     const save = (e) => {
         e.preventDefault();
-        
+
         if (title && content) {
-            dispatch(postAdded({
-                id: nanoid(),
-                title,
-                content
-            }))
+            dispatch(postAdded(title, content));
 
             setTitle('');
             setContent('');
