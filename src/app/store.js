859f2699cc9b5c95a2e1a83c0700d671f7eec907
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-09-11 20:27:52 +0630

    added usersSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { useReducer } from 'react'
 import postsSlice from './features/posts/postsSlice'
 
 export default configureStore({
   reducer: {
     posts: postsSlice,
+    users: useReducer
   }
 })
